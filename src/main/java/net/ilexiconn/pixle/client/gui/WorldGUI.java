cbc79b8816a4619b4389eb37e28aa031a17f105a
gegy1000 <gegy1000@gmail.com>
2016-03-11 07:48:35 +0200

    Fix rendering

--- src/main/java/net/ilexiconn/pixle/client/gui/WorldGUI.java
+++ src/main/java/net/ilexiconn/pixle/client/gui/WorldGUI.java
@@ -28,27 +28,28 @@ public class WorldGUI extends GUI {
             }
         }
 
-        int regionCount = Display.getWidth() / 128;
-        for (int regionX = (world.getRegionX(player.posX)) - (regionCount / 2); regionX < (world.getRegionX(player.posX)) + (regionCount / 2); regionX++) {
-            Region region = world.getRegion(regionX);
+        int regionCount = (Display.getWidth() / 128) + 1;
+        int playerRegionX = world.getRegionX(player.posX);
 
-            for (int x = 0; x < 16; x++) {
-                for (int y = 0; y < 256; y++) {
-                    int relativeY = y - player.posY;
-                    int centerY = (Display.getHeight() / 128) / 2;
+        for (int regionX = 0; regionX < regionCount; regionX++) {
+            Region region = world.getRegion((playerRegionX + regionX) - (regionCount / 2));
+            for (int y = 0; y < 256; y++) {
+                int relativeY = y - player.posY;
+                int centerY = (Display.getHeight() / 128) / 2;
 
-                    if (relativeY > -centerY && relativeY < centerY) {
+                if (relativeY > -centerY && relativeY < centerY) {
+                    for (int x = 0; x < 16; x++) {
                         Pixel pixel = region.getPixel(x, y);
                         if (pixel != null) {
                             int hex = pixel.getMaterial().getColor();
                             int r = (hex & 0xFF0000) >> 16;
                             int g = (hex & 0xFF00) >> 8;
                             int b = (hex & 0xFF);
-                            GL11.glColor3f(r / 255, g / 255, b / 255);
+                            GL11.glColor3f(r / 255.0F, g / 255.0F, b / 255.0F);
                         } else {
-                            GL11.glColor3f(0, 148 / 255, 1);
+                            GL11.glColor3f(0.0F, 148.0F / 255.0F, 1.0F);
                         }
-                        drawRect(x * 8, y * 8, 8, 8);
+                        drawRect((x + (regionX * 16)) * 8, (Display.getHeight() / 2) - (y * 8), 8, 8);
                     }
                 }
             }
