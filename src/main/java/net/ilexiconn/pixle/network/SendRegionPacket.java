f5b923e5283666ccb338321fa9515af18d885bbd
gegy1000 <gegy1000@gmail.com>
2016-03-14 20:44:45 +0200

    Port SendRegionPacket to kryonet

--- src/main/java/net/ilexiconn/pixle/network/SendRegionPacket.java
+++ src/main/java/net/ilexiconn/pixle/network/SendRegionPacket.java
@@ -1,5 +1,6 @@
 package net.ilexiconn.pixle.network;
 
+import com.esotericsoftware.kryonet.Connection;
 import net.ilexiconn.pixle.client.PixleClient;
 import net.ilexiconn.pixle.entity.PlayerEntity;
 import net.ilexiconn.pixle.level.ClientLevel;
@@ -7,88 +8,70 @@ import net.ilexiconn.pixle.level.PixelLayer;
 import net.ilexiconn.pixle.level.region.Region;
 import net.ilexiconn.pixle.server.PixleServer;
 
-import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SendRegionPacket extends PixlePacket {
-    private int[][] pixels;
+    private byte layer;
     private int regionX;
-    private PixelLayer layer;
+    private Map<Byte, Integer> ids;
+    private byte[][] pixels;
 
     public SendRegionPacket() {}
 
     public SendRegionPacket(Region region, PixelLayer layer) {
-        this.pixels = region.getPixels()[layer.ordinal()];
+        this.layer = (byte) layer.ordinal();
         this.regionX = region.getX();
-        this.layer = layer;
-    }
-
-    @Override
-    public void handleServer(PixleServer server, Socket sender, PlayerEntity player, INetworkManager networkManager, long estimatedSendTime) {
-    }
-
-    @Override
-    public void handleClient(PixleClient client, INetworkManager networkManager, long estimatedSendTime) {
-        ClientLevel level = client.getLevel();
-        Region region = level.getRegion(regionX);
-        region.setPixels(pixels, layer);
-        level.receiveRegion(region);
-    }
-
-    @Override
-    public void encode(ByteBuffer buffer) {
-        buffer.writeByte((byte) layer.ordinal());
-        buffer.writeInteger(regionX);
 
         int id = Byte.MIN_VALUE;
-        Map<Integer, Integer> ids = new HashMap<>();
+        ids = new HashMap<Byte, Integer>();
+
+        pixels = new byte[Region.REGION_WIDTH][Region.REGION_HEIGHT];
+        int[][] regionPixels = region.getPixels()[layer.ordinal()];
 
         for (int x = 0; x < pixels.length; x++) {
             for (int y = 0; y < pixels[x].length; y++) {
-                int color = pixels[x][y];
-                if (!ids.containsKey(color)) {
-                    ids.put(color, id++);
+                int color = regionPixels[x][y];
+                if (!ids.containsValue(color)) {
+                    ids.put((byte) ((id++) + Byte.MIN_VALUE), color);
                 }
             }
         }
 
-        buffer.writeByte((byte) (ids.size() + Byte.MIN_VALUE));
-
-        for (Map.Entry<Integer, Integer> entry : ids.entrySet()) {
-            buffer.writeByte((byte) (entry.getValue() + Byte.MIN_VALUE));
-            buffer.writeInteger(entry.getKey());
-        }
-
         for (int x = 0; x < pixels.length; x++) {
             for (int y = 0; y < pixels[x].length; y++) {
-                buffer.writeByte((byte) (ids.get(pixels[x][y]) + Byte.MIN_VALUE));
+                int color = regionPixels[x][y];
+                for (Map.Entry<Byte, Integer> entry : ids.entrySet()) {
+                    if (entry.getValue() == color) {
+                        pixels[x][y] = entry.getKey();
+                    }
+                }
             }
         }
     }
 
     @Override
-    public void decode(ByteBuffer buffer) {
-        layer = PixelLayer.values()[buffer.readByte()];
-        regionX = buffer.readInteger();
+    public void handleServer(PixleServer pixleServer, PlayerEntity player, Connection connection, long estimatedSendTime) {
+    }
+
+    @Override
+    public void handleClient(PixleClient client, Connection connection, long estimatedSendTime) {
+        PixelLayer layer = PixelLayer.values()[this.layer];
 
         int width = Region.REGION_WIDTH;
         int height = Region.REGION_HEIGHT;
 
-        pixels = new int[width][height];
-
-        int idCount = buffer.readByte() - Byte.MIN_VALUE;
-
-        Map<Byte, Integer> ids = new HashMap<>();
-
-        for (int i = 0; i < idCount; i++) {
-            ids.put(buffer.readByte(), buffer.readInteger());
-        }
+        int[][] pixels = new int[width][height];
 
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                pixels[x][y] = ids.get(buffer.readByte());
+                pixels[x][y] = ids.get(this.pixels[x][y]);
             }
         }
+
+        ClientLevel level = client.getLevel();
+        Region region = level.getRegion(regionX);
+        region.setPixels(pixels, layer);
+        level.receiveRegion(region);
     }
 }
